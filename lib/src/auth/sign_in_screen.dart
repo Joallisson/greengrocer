201a6be539051bc0c5bf5396f178b0e81059ec31
joallisson <joallissonnc@gmail.com>
2023-04-18 20:00:24 -0300

    9. Adicionando o nome do app e animação das categorias

--- lib/src/auth/sign_in_screen.dart
+++ lib/src/auth/sign_in_screen.dart
@@ -1,3 +1,4 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 
@@ -12,102 +13,153 @@ class SignInScreen extends StatelessWidget {
       backgroundColor: Colors.green,
       body: Column(children: [
         Expanded(
-            child: Container(
-          color: Colors.red,
-        )),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+
+              //Nome do app
+              const Text.rich(
+                TextSpan(
+                  style: TextStyle(
+                    fontSize: 40
+                  ),
+                  children: [
+                    TextSpan(
+                      text: "Green",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold
+                      )
+                    ),
+                    TextSpan(
+                      text: "grocer",
+                      style: TextStyle(
+                        color: Colors.red,
+                      )
+                    )
+                  ]
+                )
+              ),
+              
+              //Categorias
+              SizedBox(
+                height: 30,
+                child: DefaultTextStyle(
+                  style: TextStyle(
+                    fontSize: 25
+                  ),
+                  child: AnimatedTextKit(
+                    pause: Duration.zero,
+                    repeatForever: true,
+                    animatedTexts: [
+                      FadeAnimatedText("Frutas"),
+                      FadeAnimatedText("Verduras"),
+                      FadeAnimatedText("Legumes"),
+                      FadeAnimatedText("Carnes"),
+                      FadeAnimatedText("Cereais"),
+                      FadeAnimatedText("Laticíneos"),
+                    ]
+                  ),
+                ),
+              )
+            ],
+          )
+        ),
+
+        //Formulário
         Container(
           padding: const EdgeInsets.symmetric(horizontal: 32, vertical: 40),
           decoration: const BoxDecoration(
           color: Colors.white,
           borderRadius: BorderRadius.vertical(top: Radius.circular(45))),
           child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [
-          //Email
-          const CustomTextField(
-            icon: Icons.email,
-            label: 'Email',
-          ),
-
-          //Senha
-          const CustomTextField(
-            icon: Icons.lock,
-            label: 'Senha',
-            isSecret: true,
-          ),
-
-          //Botão Entrar
-          SizedBox(
-            height: 50,
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(18))),
-              onPressed: () {},
-              child: const Text(
-                "Entrar",
-                style: TextStyle(fontSize: 18),
-              ),
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            //Email
+            const CustomTextField(
+              icon: Icons.email,
+              label: 'Email',
             ),
-          ),
 
-          //Esqueceu a senha
-          Align(
-            alignment: Alignment.centerRight,
-            child: TextButton(
+            //Senha
+            const CustomTextField(
+              icon: Icons.lock,
+              label: 'Senha',
+              isSecret: true,
+            ),
+
+            //Botão Entrar
+            SizedBox(
+              height: 50,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(18))),
                 onPressed: () {},
                 child: const Text(
-                  "Esqueceu a senha?",
-                  style: TextStyle(color: Colors.red),
-                )),
-          ),
-
-          //Divisor
-          Padding(
-            padding: const EdgeInsets.only(bottom: 10),
-            child: Row(
-              children: [
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey.withAlpha(90),
-                    thickness: 2,
-                  ),
+                  "Entrar",
+                  style: TextStyle(fontSize: 18),
                 ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(horizontal: 15),
-                  child: Text("Ou"),
-                ),
-                Expanded(
-                  child: Divider(
-                    color: Colors.grey.withAlpha(90),
-                    thickness: 2,
-                  ),
-                ),
-              ],
-            ),
-          ),
-
-          //Botão criar conta
-          SizedBox(
-            height: 50,
-            child: OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18)
-                ),
-                side: const BorderSide(
-                  width: 2,
-                  color: Colors.green
-                )
               ),
-              onPressed: (){}, 
-              child: const Text("Criar conta", style: TextStyle(
-                fontSize: 18
-              ),)
             ),
-          )
 
-        ],
+            //Esqueceu a senha
+            Align(
+              alignment: Alignment.centerRight,
+              child: TextButton(
+                  onPressed: () {},
+                  child: const Text(
+                    "Esqueceu a senha?",
+                    style: TextStyle(color: Colors.red),
+                  )),
+            ),
+
+            //Divisor
+            Padding(
+              padding: const EdgeInsets.only(bottom: 10),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                  const Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 15),
+                    child: Text("Ou"),
+                  ),
+                  Expanded(
+                    child: Divider(
+                      color: Colors.grey.withAlpha(90),
+                      thickness: 2,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+
+            //Botão criar conta
+            SizedBox(
+              height: 50,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(18)
+                  ),
+                  side: const BorderSide(
+                    width: 2,
+                    color: Colors.green
+                  )
+                ),
+                onPressed: (){}, 
+                child: const Text("Criar conta", style: TextStyle(
+                  fontSize: 18
+                ),)
+              ),
+            )
+
+          ],
           ),
         )
       ]),
