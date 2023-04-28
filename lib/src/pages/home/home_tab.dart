09c90177bbef16cefb12e530057760a0bc680bef
joallisson <joallissonnc@gmail.com>
2023-04-28 10:21:19 -0300

    23. Criação do grid para apresentação dos produtos

--- lib/src/pages/home/home_tab.dart
+++ lib/src/pages/home/home_tab.dart
@@ -1,10 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom_colors.dart';
-
 import 'components/category_tile.dart';
+import 'package:greengrocer/src/config/app_data.dart' as app_data;
 
 class HomeTab extends StatefulWidget {
-  HomeTab({super.key});
+  const HomeTab({super.key});
 
   @override
   State<HomeTab> createState() => _HomeTabState();
@@ -86,20 +86,38 @@ class _HomeTabState extends State<HomeTab> {
                   return CategoryTile(
                     onPressed: () {
                       setState(() {
-                        selectedCategory = categories[index];
+                        selectedCategory = app_data.categories[index];
                       });
                     },
-                    category: categories[index],
-                    isSelected: categories[index] == selectedCategory,
+                    category: app_data.categories[index],
+                    isSelected: app_data.categories[index] == selectedCategory,
                   );
                 },
                 separatorBuilder: (_, index) => const SizedBox(
                       width: 10,
                     ),
-                itemCount: categories.length),
-          )
+                itemCount: app_data.categories.length),
+          ),
 
           //Grid
+          Expanded(
+            child: GridView.builder(
+              padding: const EdgeInsets.fromLTRB(16, 0, 16, 16),
+              physics: const BouncingScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                mainAxisSpacing: 10,
+                crossAxisSpacing: 10,
+                childAspectRatio: 9 / 11.5,
+              ),
+              itemCount: app_data.items.length,
+              itemBuilder: (_,index){
+                return Container(
+                  color: Colors.red,
+                );
+              }
+            ),
+          )
         ],
       ),
     );
