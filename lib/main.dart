3277e0b0cfeb2fb70d9f2f8b5704229d2f2be5b5
Joallisson <joallisson@gmail.com>
2024-02-09 20:29:36 -0300

    Definido que o nosso app só deve rodar em pé

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:greengrocer/src/pages/auth/controller/auth_controller.dart';
 import 'package:greengrocer/src/pages_routes/app_pages.dart';
@@ -8,7 +9,11 @@ void main() {
 
   Get.put(AuthController());
 
-  runApp(const MyApp());
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then((_) {
+    runApp(const MyApp());
+  });
 }
 
 class MyApp extends StatelessWidget {
@@ -20,9 +25,8 @@ class MyApp extends StatelessWidget {
     return GetMaterialApp(
       title: 'Greengrocer',
       theme: ThemeData(
-        primarySwatch: Colors.green,
-        scaffoldBackgroundColor: Colors.white.withAlpha(190)
-      ),
+          primarySwatch: Colors.green,
+          scaffoldBackgroundColor: Colors.white.withAlpha(190)),
       debugShowCheckedModeBanner: false,
       initialRoute: PagesRoutes.splashRoute,
       getPages: AppPages.pages,
